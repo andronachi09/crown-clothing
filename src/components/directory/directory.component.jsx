2627d32c65a623c71ed9d6704a4adebf135b87fc
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2023-10-03 16:10:23 +0200

    feat: navigation bar with React Route

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,18 +2,19 @@ import Category from "../category-item/category-item.component";
 
 import "../directory/directory.styles.scss"
 
-const Directory = ({ categories}) => {
+const Directory = ({ categories }) => {
 
-    return (
+  return (
     <div className="directory-container">
       {
-      categories.map(( category ) => {
-        return (
-          <Category key={ category.id } category={category}/>
-        )})
+        categories.map((category) => {
+          return (
+            <Category key={category.id} category={category} />
+          )
+        })
       }
     </div>
-    );
-}
+  );
+};
 
 export default Directory;

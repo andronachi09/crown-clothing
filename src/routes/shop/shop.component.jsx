f8b4a465ebef9bc3c2a37bd0aabf00b71bd67d9a
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2023-11-01 20:15:50 +0100

    feat: routes for shop

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,20 +1,16 @@
-import { useContext } from "react";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
+import { Routes, Route } from "react-router-dom";
 
-import { CategoriesContext } from "../../contexts/categories.context";
+import Category from "../category/category.component";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
 
 import "./shop.styles.scss";
 
 const Shop = () => {
-    const { categoriesMap } = useContext(CategoriesContext);
-
     return (
-        <div className="shop-container">
-            {Object.keys(categoriesMap).map(title => {
-                const products = categoriesMap[title];
-                return <CategoryPreview key={ title } products={ products } title={ title }> </CategoryPreview>
-            })}
-        </div>
+        <Routes>
+            <Route index element={< CategoriesPreview />}/>
+            <Route path=":category" element={ <Category /> }/>
+        </Routes>
     );
 };
 

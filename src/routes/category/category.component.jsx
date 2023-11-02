bf059cd8c79ba560b9ea5026f12333ad11361fd6
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2023-11-02 12:12:35 +0100

    feat: Routing ffrom shop page to categories pages

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,5 +1,5 @@
+import { useContext, useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
-import { useContext, useState, useEffect } from "react";
 
 import ProductCard from "../../components/product-card/product-card.component";
 
@@ -10,20 +10,21 @@ import "./category.styles.scss";
 const Category = () => {
     const { category } = useParams();
     const { categoriesMap } = useContext(CategoriesContext);
-    const [products, setProducts] = useState(categoriesMap[category]);
+    const [ products, setProducts ] = useState(categoriesMap[category]);
 
     useEffect(() => {
         setProducts(categoriesMap[category]);
     }, [category, categoriesMap]);
 
     return (
-        <div className="category-container">
-            { products &&
-                products.map((product) =>
-                    <ProductCard key={ product.id } product={product} />
-                )
-            }
-        </div>
+        <Fragment>
+            <h2 className="category-title">{ category.toUpperCase() }</h2>
+            <div className="category-container">
+                { products &&
+                    products.map((product) => <ProductCard key={ product.id } product={product} />)
+                }
+            </div>
+        </Fragment>
     );
 };
 

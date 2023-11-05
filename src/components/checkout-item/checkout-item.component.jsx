6b9d69650a666bbc96fac42d66584805935cfc9e
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2023-11-05 14:34:39 +0100

    chore: CheckoutItemComponent styled

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 
 import { CartContext } from "../../contexts/cart.context";
 
-import "./checkout-item.styles.scss"
+import { CheckoutItemContainer, ImageContainer, Value, Quantity, Arrow, QuantityValue, RemoveButton } from "./checkout-item.styles";
 
 const CheckoutItem = ({ cartItem }) => {
     const { name, quantity, price, imageUrl } = cartItem;
@@ -13,23 +13,23 @@ const CheckoutItem = ({ cartItem }) => {
     const decreaseItemQuantityHandler = () => decreaseProductQuantity(cartItem);
 
     return (
-        <div className="checkout-item-container">
-            <div className="image-container">
+        <CheckoutItemContainer>
+            <ImageContainer>
                 <img src={ imageUrl } alt={ `${ name }` } />
-            </div>
-            <span className="name">{ name }</span>
-            <span className="quantity">
-                <div className="arrow" onClick={decreaseItemQuantityHandler}>
+            </ImageContainer>
+            <Value>{ name }</Value>
+            <Quantity>
+                <Arrow onClick={decreaseItemQuantityHandler}>
                     &#10094;
-                </div>
-                <span className="value">{ quantity }</span>
-                <div className="arrow" onClick={ increaseItemQuantityHandler }>
+                </Arrow>
+                <QuantityValue>{ quantity }</QuantityValue>
+                <Arrow onClick={ increaseItemQuantityHandler }>
                     &#10095;
-                </div>
-            </span>
-            <span className="price">{ price }</span>
-            <div className="remove-button" onClick={ deleteItemHandler }>&#10005;</div>
-        </div>
+                </Arrow>
+            </Quantity>
+            <Value>{ price }</Value>
+            <RemoveButton onClick={ deleteItemHandler }>&#10005;</RemoveButton>
+        </CheckoutItemContainer>
     );
 };
 
---
+++ src/components/checkout-item/checkout-item.styles.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const CheckoutItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+  min-height: 100px;
+  border-bottom: 1px solid darkgrey;
+  padding: 15px 0;
+  font-size: 20px;
+  align-items: center;
+`;
+
+export const ImageContainer = styled.div`
+  width: 23%;
+  padding-right: 15px;
+`;
+
+export const Value = styled.span`
+ width: 23%;
+`;
+
+export const Quantity = styled.span`
+display: flex;
+ width: 23%;
+`;
+
+export const Arrow = styled.div`
+  cursor: pointer;
+`;
+
+export const QuantityValue = styled.div`
+  margin: 0 10px;
+`;
+
+export const RemoveButton = styled.div`
+  padding-left: 12px;
+  cursor: pointer;
+`;

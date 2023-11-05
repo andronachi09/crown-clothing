bbec95d6dae0304b05b2e80af879097986357c2b
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2023-11-05 16:12:37 +0100

    chore: DirectoryItemComponent  styled(also used one prop)

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,18 +1,15 @@
-import "./directory-item.styles.scss"
+import { Body, BackgroundImage, DirectoryItemContainer } from "./directory-item.styles"
 
 const DirectoryItem = ({ category }) => {
     const { title, imageUrl } = category;
     return (
-        <div className="directory-item-container">
-            <div className="background-image" style={{
-                backgroundImage: `url(${imageUrl})`
-            }}>
-            </div>
-            <div className="body">
+        <DirectoryItemContainer>
+            <BackgroundImage imageUrl={ imageUrl }/>
+            <Body>
                 <h2>{title}</h2>
                 <p>Shop now!</p>
-            </div>
-        </div>
+            </Body>
+        </DirectoryItemContainer>
     );
 };
 

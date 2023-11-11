2505313aec1e8fa0b155be647b3a3aea7bb2ca4e
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2023-11-11 19:46:53 +0100

    chore: FormInput component styled

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,15 +1,17 @@
-import "../form-input/form-input.style.scss"
+import { FormInputLabel, Group, Input } from "./form-input.style.jsx";
 
 const FormInput = ({ label, ...otherProps }) => {
     return (
-        <div className="group">
-            <input className="form-input" {...otherProps} />
+        <Group>
+            <Input {...otherProps} />
             {
                 label && (
-                    <label className={ `${otherProps.value.length ? 'shrink' : ''} form-input-label`}>{label}</label>
+                    <FormInputLabel shrink={ otherProps.value.length }>
+                        {label}
+                    </FormInputLabel>
                 )
             }
-        </div>
+        </Group>
     )
 };
 

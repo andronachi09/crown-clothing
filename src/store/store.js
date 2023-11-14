df9ecbd956e65a702d525c49cab04eb1dc633492
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2023-11-14 22:22:52 +0100

    feat: setting up the store for redux

---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+// here we eill generate the store object that we will use inside application
+import { compose, createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+
+import { rootReducer } from "./root-reducer";
+
+//root-reducer, a combination of all reducers. Reducers are pure functions.
+//middle wares a kind of little library helpers, that run before an action hits the reducer.
+//whenever we dispatch an action, before action hits the reducers, it hits the middl eware first
+const middleWares = [logger];
+
+const composeEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, middleWares);

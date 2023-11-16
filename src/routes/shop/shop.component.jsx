5654201e2687c71a83b3a453f54938b56eb76407
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2023-11-16 22:52:11 +0100

    feat: categories reducer and selector

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,25 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { setCategoriesMap } from "../../store/categories/categories.action";
 
 import Category from "../category/category.component";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 
 const Shop = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+    const getCategoriesMap = async () => {
+        const categoryMap = await getCategoriesAndDocuments();
+        dispatch(setCategoriesMap(categoryMap));
+    };
+
+    getCategoriesMap();
+}, [dispatch]);
+
     return (
         <Routes>
             <Route index element={< CategoriesPreview />}/>

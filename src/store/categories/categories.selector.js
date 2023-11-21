5c1d2a561ac4a30571c2cb4f116182d4b531f69f
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2023-11-22 00:46:23 +0100

    chore: reselect for categories selector

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,6 +1,26 @@
-// from array to map
-export const selectCategoriesMap = (state) => state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-}, {});
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => {
+    console.log("first selector fired");
+    return state.categories;
+};
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => {
+        console.log("second selector fired");
+        return categoriesSlice.categories
+    }
+);
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => {
+        console.log("second selector fired");
+        return categories.reduce((acc, category) => {
+            const { title, items } = category;
+            acc[title.toLowerCase()] = items;
+            return acc;
+        }, {})
+    }
+);

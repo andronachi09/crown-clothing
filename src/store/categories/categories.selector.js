132dd05f95765fc4717886ed984aff179b1a4082
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2023-11-21 23:26:18 +0100

    chore: comment

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,3 +1,4 @@
+// from array to map
 export const selectCategoriesMap = (state) => state.categories.categories.reduce((acc, category) => {
     const { title, items } = category;
     acc[title.toLowerCase()] = items;

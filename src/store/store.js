066c749db1b2eb838e988689f072ffc193f6ef28
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2023-11-21 23:26:00 +0100

    chore: creating own logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,27 @@
 // here we eill generate the store object that we will use inside application
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
+
+const loggerMiddleware = (store) => (next) => (action) => {
+    if (!action.type) {
+        return next(action)
+    };
+
+    console.log('type: ', action.type);
+    console.log('payload: ', action.payload);
+    console.log('currentState: ', store.getState());
+
+    next(action);
+
+    console.log('next state: ', store.getState());
+};
 
 //root-reducer, a combination of all reducers. Reducers are pure functions.
 //middle wares a kind of little library helpers, that run before an action hits the reducer.
 //whenever we dispatch an action, before action hits the reducers, it hits the middl eware first
-const middleWares = [ logger ];
+const middleWares = [ loggerMiddleware ];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 

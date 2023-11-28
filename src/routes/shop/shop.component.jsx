309268a20df4708140ef3b725835e10b89c998ca
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2023-11-28 21:21:31 +0100

    chore: fetchCategoriesAsync Thunk to Saga

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
-import { fetchCategoriesStartAsync } from "../../store/categories/categories.action";
+import { fetchCategoriesStart } from "../../store/categories/categories.action";
 
 import Category from "../category/category.component";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
@@ -11,8 +11,7 @@ const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        console.log('hai pfidar');
-        dispatch(fetchCategoriesStartAsync());
+        dispatch(fetchCategoriesStart());
     }, []);
 
     return (
---
+++ src/store/categories/categories.saga.js
@@ -0,0 +1,26 @@
+import { takeLatest, all, call, put } from "redux-saga/effects";
+
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+
+import { fetchCategoriesSuccess, fetchCategoriesFailed } from "./categories.action";
+
+import { CATEGORIES_ACTION_TYPES } from "./categories.types";
+
+export  function* fetchCategoriesAsync() {
+    try {
+        const categoriesArray = yield call(getCategoriesAndDocuments, 'categories'); //call when we need to fetch something
+        yield put(fetchCategoriesSuccess(categoriesArray)) //put = generator version dispatch
+    } catch (error) {
+        yield put(fetchCategoriesFailed(error));
+    }
+}
+
+export function* onFetchCategories() {
+    yield takeLatest(
+        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START,
+        fetchCategoriesAsync) //takeLatest - latest action type
+};
+
+export function* categoriesSaga() {
+    yield all([call(onFetchCategories)])
+};
